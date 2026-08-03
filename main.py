989f91b7d92aad44d7c72dc6935e6b3a69801422
harshahs2004 <hemanthhs55555@gmail.com>
2026-08-03 23:23:28 +0530

    data validation completed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+from networksecurity.components.data_validation import DataValidation
 import sys
 
 if __name__=="__main__":
@@ -12,5 +13,18 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("initated the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("data ingestion completed")
+        datavalidation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validataion=DataValidation(dataingestionartifact,datavalidation_config)
+        logging.info("initated the data validation")
+        data_validation_artifact=data_validataion.initiate_data_validation()
+        logging.info("data validation completed")
+        print(data_validation_artifact)
+        
+
+
+
+
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -3,3 +3,16 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path:str
     test_file_path:str
+
+@dataclass 
+class DataValidationArtifact:
+    validation_status: bool
+    valid_train_file_path: bool 
+    valid_test_file_path: bool
+    invalid_train_file_path: bool 
+    invalid_test_file_path: bool
+    drift_report_file_path: str
+    
+    
+   
+
