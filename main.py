89e23ed1a0da15010fdbd6f672768f1397a417e2
harshahs2004 <hemanthhs55555@gmail.com>
2026-08-06 00:04:19 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__=="__main__":
@@ -20,6 +21,12 @@ if __name__=="__main__":
         data_validation_artifact=data_validataion.initiate_data_validation()
         logging.info("data validation completed")
         print(data_validation_artifact)
+        logging.info("data transformation started")
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation completed")
         
 
 
--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -7,12 +7,17 @@ class DataIngestionArtifact:
 @dataclass 
 class DataValidationArtifact:
     validation_status: bool
-    valid_train_file_path: bool 
-    valid_test_file_path: bool
-    invalid_train_file_path: bool 
-    invalid_test_file_path: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
     drift_report_file_path: str
     
-    
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
    
 
